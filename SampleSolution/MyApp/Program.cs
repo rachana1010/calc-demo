87773e623ec7d949f51a2edaa24b15bd520d882f
Rachana S <rachana.s@planview.com>
2024-08-28 12:38:45 +0530

    first commit

---
+++ SampleSolution/MyApp/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MyApp;
+
+Console.WriteLine("Hello, World!");
+
+Calculator calculator= new Calculator();
+Console.WriteLine(calculator.Add(3, 5));
